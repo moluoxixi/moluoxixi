ed3d545b65cd4262a8c96dbbbe1d79947af8bfa1
wanglun <wrnm.@.com>
2025-02-13 17:03:10 +0800

    feat: my first commit

---
+++ scripts/generate.ts
@@ -0,0 +1,34 @@
+import consola from "consola";
+
+import fs from "fs";
+import { markdown } from "@yunyoujun/utils";
+import { generateGithubStatsImg, generateIconList } from "./utils";
+import { iconsList } from "../data/iconsList";
+
+const readmeFile = "README.md";
+let readmeContent = fs.readFileSync(readmeFile, "utf-8");
+
+iconsList.forEach((item) => {
+  readmeContent = markdown.injectContentBetweenTags(
+    item.namespace,
+    readmeContent,
+    generateIconList(item.icons)
+  );
+});
+consola.success(`Generate ${readmeFile} successfully!`);
+
+readmeContent = markdown.injectContentBetweenTags(
+  'github-stats',
+  readmeContent,
+  generateGithubStatsImg({
+    username: 'moluoxixi',
+    show_icons: true,
+    icon_color: '0078e7',
+    title_color: '0078e7',
+    include_all_commits: true
+  }),
+)
+consola.success(`Generate github-stats successfully!`);
+
+if (readmeContent)
+  fs.writeFileSync(readmeFile, readmeContent);
